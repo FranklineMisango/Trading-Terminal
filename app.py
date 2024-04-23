4e1e55a61f6984f342610738de133461f302f110
misango <franklinemisango@gmail.com>
2024-04-23 23:59:44 +0800

    Finalized fivviz bp addition

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import pandas as pd
 import numpy as np
 import plotly.figure_factory as ff
 import tickers as ti
+import pandas as pd
+from bs4 import BeautifulSoup as soup
+from urllib.request import Request, urlopen
 
 st.title('Comprehensive Lite Algorithmic Trading Terminal : Identify, Visualize, Predict and Trade')
 st.sidebar.info('Welcome to my Algorithmic Trading App Choose your options below')
@@ -62,7 +65,7 @@ def main():
                 end_date = st.date_input("End Date:")
             st.title('Correlation Viewer for Stocks')
             
-            # Define tickers by sector based on the breakdown provided
+            # Add more stocks to the portfolio
             sectors = {
                 "Technology": ['AAPL', 'MSFT', 'GOOGL', 'META', 'NVDA', 'INTC', 'CSCO', 'ADBE', 'AVGO', 'PYPL'],
                 "Health Care": ['JNJ', 'PFE', 'UNH', 'MRK', 'ABBV', 'TMO', 'MDT', 'DHR', 'AMGN', 'LLY'],
@@ -84,6 +87,46 @@ def main():
                 corr_matrix = correlated_stocks(start_date, end_date, tickers)
                 print("After computing correlation matrix")
                 visualize_correlation_heatmap(corr_matrix)
+        if options == "Finviz_growth_screener":
+            # Set display options for pandas DataFrame
+            pd.set_option('display.max_columns', None)
+            pd.set_option('display.max_rows', None)
+
+            def growth_screener():
+                try:
+                    frames = []
+                    # Loop through different pages of FinViz growth screener
+                    for i in range(1, 105, 20):
+                        url = (f"https://finviz.com/screener.ashx?v=151&f=fa_epsqoq_o15,fa_epsyoy_pos,fa_epsyoy1_o25,fa_grossmargin_pos,fa_salesqoq_o25,ind_stocksonly,sh_avgvol_o300,sh_insttrans_pos,sh_price_o10,ta_perf_52w50o,ta_sma200_pa,ta_sma50_pa&ft=4&o=-marketcap&r=0{i}")
+                        req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+                        webpage = urlopen(req).read()
+                        html = soup(webpage, "html.parser")
+
+                        # Extracting stock data from the HTML
+                        stocks = pd.read_html(str(html))[-2]
+                        stocks = stocks.set_index('Ticker')
+                        frames.append(stocks)
+
+                    # Concatenate all DataFrame objects from different pages
+                    df = pd.concat(frames)
+                    df = df.drop_duplicates()
+                    df = df.drop(columns=['No.'])
+                    return df
+                except Exception as e:
+                    print(f"Error occurred: {e}")
+                    return pd.DataFrame()
+
+            # Execute the screener and store the result
+            df = growth_screener()
+
+            # Display the results
+            st.write('\nGrowth Stocks Screener:')
+            st.write(df)
+
+            # Extract and print list of tickers
+            tickers = df.index
+            st.write('\nList of Tickers:')
+            st.write(tickers)
 
     elif option == 'Recent Data':
         pass
