8afa46773406e4154265fbc565ce5fd0c92d18af
misango <franklinemisango@gmail.com>
2024-05-12 13:06:35 +0800

    added the RSI snippet

--- app.py
+++ app.py
@@ -6278,7 +6278,7 @@ def main():
                 st.plotly_chart(fig2)
 
         if pred_option_Technical_Indicators == "Return on Investment (ROI)":
-            st.success("This program allows you to view the ROC of a ticker")
+            st.success("This program allows you to view the ROI over time of a ticker")
             ticker = st.text_input("Enter the ticker you want to monitor")
             if ticker:
                 message = (f"Ticker captured : {ticker}")
@@ -6339,8 +6339,73 @@ def main():
                 st.plotly_chart(fig2)
 
                 
-        if pred_option_Technical_Indicators == "RSI":
-            pass
+        if pred_option_Technical_Indicators == "Relative Strength Index (RSI)":
+            st.success("This program allows you to view the RSI over time of a ticker")
+            ticker = st.text_input("Enter the ticker you want to monitor")
+            if ticker:
+                message = (f"Ticker captured : {ticker}")
+                st.success(message)
+            col1, col2 = st.columns([2, 2])
+            with col1:
+                start_date = st.date_input("Start date:")
+            with col2:
+                end_date = st.date_input("End Date:")
+            if st.button("Check"):    
+                symbol = ticker
+                start = start_date
+                end = end_date
+                # Read data
+                df = yf.download(symbol, start, end)
+
+                n = 14  # Number of period
+                change = df["Adj Close"].diff(1)
+                df["Gain"] = change.mask(change < 0, 0)
+                df["Loss"] = abs(change.mask(change > 0, 0))
+                df["AVG_Gain"] = df.Gain.rolling(n).mean()
+                df["AVG_Loss"] = df.Loss.rolling(n).mean()
+                df["RS"] = df["AVG_Gain"] / df["AVG_Loss"]
+                df["RSI"] = 100 - (100 / (1 + df["RS"]))
+
+                # Create RSI plot
+                fig1 = go.Figure()
+                fig1.add_trace(go.Scatter(x=df.index, y=df["Adj Close"], mode='lines', name='Adj Close'))
+
+                fig1.add_trace(go.Scatter(x=df.index, y=df["RSI"], mode='lines', name='Relative Strength Index'))
+
+                fig1.update_layout(title=symbol + " Closing Price and Relative Strength Index (RSI)",
+                                xaxis_title="Date",
+                                yaxis_title="Price/RSI")
+
+                st.plotly_chart(fig1)
+
+                # Candlestick with RSI
+                dfc = df.copy()
+                dfc["VolumePositive"] = dfc["Open"] < dfc["Adj Close"]
+                dfc = dfc.reset_index()
+                dfc["Date"] = pd.to_datetime(dfc["Date"])
+                dfc["Date"] = dfc["Date"].apply(mdates.date2num)
+
+                fig2 = go.Figure()
+
+                # Candlestick chart
+                fig2.add_trace(go.Candlestick(x=dfc['Date'],
+                                open=dfc['Open'],
+                                high=dfc['High'],
+                                low=dfc['Low'],
+                                close=dfc['Adj Close'], name='Candlestick'))
+
+                # Volume bars
+                fig2.add_trace(go.Bar(x=dfc['Date'], y=dfc['Volume'], marker_color=dfc.VolumePositive.map({True: "green", False: "red"}), name='Volume'))
+
+                fig2.add_trace(go.Scatter(x=df.index, y=df["RSI"], mode='lines', name='Relative Strength Index', line=dict(color='blue')))
+
+                fig2.update_layout(title=symbol + " Candlestick Chart with Relative Strength Index (RSI)",
+                                xaxis_title="Date",
+                                yaxis_title="Price",
+                                xaxis_rangeslider_visible=False)
+
+                st.plotly_chart(fig2)
+
         if pred_option_Technical_Indicators == "RSI_Bollinger Bands":
             pass
         if pred_option_Technical_Indicators == "Volume Weighted Average Price (VWAP)":
