58817d4840e6b6afa548e15674acc80a0c38b3d0
misango <franklinemisango@gmail.com>
2024-05-12 12:25:57 +0800

    added the PVI snippet

--- app.py
+++ app.py
@@ -5499,7 +5499,6 @@ def main():
                                                                   'GMMA',
                                                                   'Moving Average Convergence Divergence (MACD)',
                                                                   'MA high low',
-                                                                  'MFI',
                                                                   'Price Volume Trend Indicator (PVI)',
                                                                   'Price Volume Trend (PVT)',
                                                                   'Rate of Change (ROC)',
@@ -6084,7 +6083,81 @@ def main():
                                 yaxis_title="Price")
 
                 st.plotly_chart(fig2)
+        if pred_option_Technical_Indicators == "Price Volume Trend Indicator (PVI)":
+            st.success("This program allows you to view the MA High/low of a ticker")
+            ticker = st.text_input("Enter the ticker you want to monitor")
+            if ticker:
+                message = (f"Ticker captured : {ticker}")
+                st.success(message)
+            col1, col2 = st.columns([2, 2])
+            with col1:
+                start_date = st.date_input("Start date:")
+            with col2:
+                end_date = st.date_input("End Date:")
+            if st.button("Check"):    
+                symbol = ticker
+                start = start_date
+                end = end_date  
+                # Read data
+                df = yf.download(symbol, start, end)
 
+                returns = df["Adj Close"].pct_change()
+                vol_increase = df["Volume"].shift(1) < df["Volume"]
+                pvi = pd.Series(data=np.nan, index=df["Adj Close"].index, dtype="float64")
+
+                pvi.iloc[0] = 1000
+                for i in range(1, len(pvi)):
+                    if vol_increase.iloc[i]:
+                        pvi.iloc[i] = pvi.iloc[i - 1] * (1.0 + returns.iloc[i])
+                    else:
+                        pvi.iloc[i] = pvi.iloc[i - 1]
+
+                pvi = pvi.replace([np.inf, -np.inf], np.nan).fillna(1000)
+
+                df["PVI"] = pd.Series(pvi)
+
+                # Line Chart with PVI
+                fig1 = go.Figure()
+                fig1.add_trace(go.Scatter(x=df.index, y=df["Adj Close"], mode='lines', name='Adj Close'))
+                fig1.add_trace(go.Scatter(x=df.index, y=df["PVI"], mode='lines', name='Positive Volume Index'))
+
+                fig1.update_layout(title="Adj Close and Positive Volume Index (PVI) Over Time",
+                                xaxis_title="Date",
+                                yaxis_title="Price/PVI")
+
+                st.plotly_chart(fig1)
+
+                # Candlestick with PVI
+                dfc = df.copy()
+                dfc["VolumePositive"] = dfc["Open"] < dfc["Adj Close"]
+                dfc = dfc.reset_index()
+                dfc["Date"] = mdates.date2num(dfc["Date"].tolist())
+
+                fig2 = go.Figure()
+
+                # Candlestick chart
+                fig2.add_trace(go.Candlestick(x=dfc['Date'],
+                                open=dfc['Open'],
+                                high=dfc['High'],
+                                low=dfc['Low'],
+                                close=dfc['Adj Close'], name='Candlestick'))
+
+                # Volume bars
+                fig2.add_trace(go.Bar(x=dfc['Date'], y=dfc['Volume'], marker_color=dfc.VolumePositive.map({True: "green", False: "red"}), name='Volume'))
+
+                fig2.add_trace(go.Scatter(x=df.index, y=df["PVI"], mode='lines', name='Positive Volume Index', line=dict(color='green')))
+
+                fig2.update_layout(title="Candlestick Chart with Positive Volume Index (PVI)",
+                                xaxis_title="Date",
+                                yaxis_title="Price",
+                                xaxis_rangeslider_visible=False)
+
+                st.plotly_chart(fig2)
+
+        if pred_option_Technical_Indicators == "Positive Volume Trend(PVT)":
+            pass
+        if pred_option_Technical_Indicators == "ROC":
+            pass
         if pred_option_Technical_Indicators == "Volume Weighted Average Price (VWAP)":
             pass
         if pred_option_Technical_Indicators == "Weighted Moving Average (WMA)":
