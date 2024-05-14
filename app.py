4c5d4622a295d4517787c3d459db324fe8f014f7
misango <franklinemisango@gmail.com>
2024-05-14 11:01:15 +0800

    added aaron oscillator

--- app.py
+++ app.py
@@ -6966,7 +6966,93 @@ def main():
 
 
         if pred_option_Technical_Indicators == "Aroon Oscillator":
-            pass
+            st.success("This program allows you to view the Acclerations bands of a ticker over time")
+            ticker = st.text_input("Enter the ticker you want to monitor")
+            if ticker:
+                message = (f"Ticker captured : {ticker}")
+                st.success(message)
+            col1, col2 = st.columns([2, 2])
+            with col1:
+                start_date = st.date_input("Start date:")
+            with col2:
+                end_date = st.date_input("End Date:")
+            if st.button("Check"):    
+                symbol = ticker
+                start = start_date
+                end = end_date
+                # Read data
+                df = yf.download(symbol, start, end)
+
+                n = 25
+                high_max = lambda xs: np.argmax(xs[::-1])
+                low_min = lambda xs: np.argmin(xs[::-1])
+
+                df["Days since last High"] = (
+                    df["High"]
+                    .rolling(center=False, min_periods=0, window=n)
+                    .apply(func=high_max)
+                    .astype(int)
+                )
+
+                df["Days since last Low"] = (
+                    df["Low"]
+                    .rolling(center=False, min_periods=0, window=n)
+                    .apply(func=low_min)
+                    .astype(int)
+                )
+
+                df["Aroon_Up"] = ((25 - df["Days since last High"]) / 25) * 100
+                df["Aroon_Down"] = ((25 - df["Days since last Low"]) / 25) * 100
+
+                df["Aroon_Oscillator"] = df["Aroon_Up"] - df["Aroon_Down"]
+
+                df = df.drop(
+                    ["Days since last High", "Days since last Low", "Aroon_Up", "Aroon_Down"], axis=1
+                )
+
+                # Line Chart
+                fig = go.Figure()
+                fig.add_trace(go.Scatter(x=df.index, y=df["Adj Close"], mode='lines', name='Adj Close'))
+                fig.update_layout(title="Stock " + symbol + " Closing Price",
+                                xaxis_title="Date",
+                                yaxis_title="Price",
+                                legend=dict(x=0, y=1, traceorder="normal"))
+
+                fig.add_trace(go.Scatter(x=df.index, y=df["Aroon_Oscillator"], mode='lines', name='Aroon Oscillator', line=dict(color='green')))
+                fig.add_trace(go.Scatter(x=df.index, y=[0]*len(df), mode='lines', name='Zero Line', line=dict(color='red', dash='dash')))
+                st.plotly_chart(fig)
+
+                # Bar Chart
+                fig = go.Figure()
+                df["Positive"] = df["Aroon_Oscillator"] > 0
+                fig.add_trace(go.Bar(x=df.index, y=df["Aroon_Oscillator"], marker_color=df.Positive.map({True: "green", False: "red"})))
+                fig.add_shape(type="line", x0=df.index[0], y0=0, x1=df.index[-1], y1=0, line=dict(color="red", width=1, dash="dash"))
+
+                fig.update_layout(title="Aroon Oscillator",
+                                xaxis_title="Date",
+                                yaxis_title="Aroon Oscillator",
+                                legend=dict(x=0, y=1, traceorder="normal"))
+
+                st.plotly_chart(fig)
+
+                # Candlestick Chart
+                fig = go.Figure()
+
+                fig.add_trace(go.Candlestick(x=df.index,
+                                open=df['Open'],
+                                high=df['High'],
+                                low=df['Low'],
+                                close=df['Close'], name='Candlestick'))
+
+                fig.update_layout(title="Stock " + symbol + " Closing Price",
+                                xaxis_title="Date",
+                                yaxis_title="Price",
+                                legend=dict(x=0, y=1, traceorder="normal"))
+
+                fig.add_trace(go.Scatter(x=df.index, y=df["Aroon_Oscillator"], mode='lines', name='Aroon Oscillator', line=dict(color='green')))
+                fig.add_trace(go.Scatter(x=df.index, y=[0]*len(df), mode='lines', name='Zero Line', line=dict(color='red', dash='dash')))
+                st.plotly_chart(fig)
+
         if pred_option_Technical_Indicators == "Average Directional Index (ADX)":
             pass
         if pred_option_Technical_Indicators == "Average True Range (ATR)":
