0c1625d2234b6f9ea2fdd3407f2d0c22764da9f8
misango <franklinemisango@gmail.com>
2024-05-15 11:15:00 +0800

    added breadth indicator;corrected chaikin and cvi

--- app.py
+++ app.py
@@ -7429,6 +7429,38 @@ def main():
                 # Read data
                 df = yf.download(symbol, start, end)
 
+                #df["Adj Close"][1:]
+
+                # ## On Balance Volume
+                OBV = ta.OBV(df["Adj Close"], df["Volume"])
+
+                Advances = q.get("URC/NYSE_ADV", start_date="2017-07-27")["Numbers of Stocks"]
+                Declines = q.get("URC/NYSE_DEC", start_date="2017-07-27")["Numbers of Stocks"]
+
+                adv_vol = q.get("URC/NYSE_ADV_VOL", start_date="2017-07-27")["Numbers of Stocks"]
+                dec_vol = q.get("URC/NYSE_DEC_VOL", start_date="2017-07-27")["Numbers of Stocks"]
+
+                data = pd.DataFrame()
+                data["Advances"] = Advances
+                data["Declines"] = Declines
+                data["adv_vol"] = adv_vol
+                data["dec_vol"] = dec_vol
+
+                data["Net_Advances"] = data["Advances"] - data["Declines"]
+                data["Ratio_Adjusted"] = (
+                    data["Net_Advances"] / (data["Advances"] + data["Declines"])
+                ) * 1000
+                data["19_EMA"] = ta.EMA(data["Ratio_Adjusted"], timeperiod=19)
+                data["39_EMA"] = ta.EMA(data["Ratio_Adjusted"], timeperiod=39)
+                data["RANA"] = (
+                    (data["Advances"] - data["Declines"]) / (data["Advances"] + data["Declines"]) * 1000
+                )
+
+                # Finding the TRIN Value
+                data["ad_ratio"] = data["Advances"].divide(data["Declines"])  # AD Ratio
+                data["ad_vol"] = data["adv_vol"].divide(data["dec_vol"])  # AD Volume Ratio
+                data["TRIN"] = data["ad_ratio"].divide(data["adv_vol"])  # TRIN Value
+
                 # Function to calculate Force Index
                 def ForceIndex(data, n):
                     ForceIndex = pd.Series(df["Adj Close"].diff(n) * df["Volume"], name="ForceIndex")
@@ -7459,15 +7491,23 @@ def main():
 
                 # Function to calculate Chaikin Oscillator
                 def Chaikin(data):
-                    money_flow_volume = ((2 * df["Adj Close"] - df["High"] - df["Low"]) / (df["High"] - df["Low"]) * df["Volume"])
+                    money_flow_volume = (
+                        (2 * data["Adj Close"] - data["High"] - data["Low"])
+                        / (data["High"] - data["Low"])
+                        * data["Volume"]
+                    )
                     ad = money_flow_volume.cumsum()
-                    Chaikin = pd.Series(ad.ewm(com=(3 - 1) / 2).mean() - ad.ewm(com=(10 - 1) / 2).mean(), name="Chaikin")
-                    data = data.join(Chaikin)
+                    Chaikin = pd.Series(
+                        ad.ewm(com=(3 - 1) / 2).mean() - ad.ewm(com=(10 - 1) / 2).mean(), name="Chaikin"
+                    )
+                    #data["Chaikin"] = data.join(Chaikin)
+                    data["Chaikin"] = Chaikin
                     return data
+
 
                 # Calculate Chaikin Oscillator
                 Chaikin(df)
-                st.write(df)
+              
 
                 # Chaikin Oscillator Plot
                 fig = go.Figure()
