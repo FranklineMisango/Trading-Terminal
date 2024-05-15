5f2f3feb7e653c7f078168b36b91d68ce48fd302
misango <franklinemisango@gmail.com>
2024-05-15 10:50:14 +0800

    added bollinger bandwidth

--- app.py
+++ app.py
@@ -7358,7 +7358,57 @@ def main():
                 st.plotly_chart(fig)
 
         if pred_option_Technical_Indicators == "Bollinger Bandwidth":
-            pass
+            st.success("This program allows you to view the Acclerations bands of a ticker over time")
+            ticker = st.text_input("Enter the ticker you want to monitor")
+            if ticker:
+                message = (f"Ticker captured : {ticker}")
+                st.success(message)
+            col1, col2 = st.columns([2, 2])
+            with col1:
+                start_date = st.date_input("Start date:")
+            with col2:
+                end_date = st.date_input("End Date:")
+            if st.button("Check"):    
+                symbol = ticker
+                start = start_date
+                end = end_date
+
+                # Read data
+                df = yf.download(symbol, start, end)
+
+                n = 20
+                MA = pd.Series(df["Adj Close"].rolling(n).mean())
+                STD = pd.Series(df["Adj Close"].rolling(n).std())
+                bb1 = MA + 2 * STD
+                df["Upper Bollinger Band"] = pd.Series(bb1)
+                bb2 = MA - 2 * STD
+                df["Lower Bollinger Band"] = pd.Series(bb2)
+                df["SMA"] = df["Adj Close"].rolling(n).mean()
+
+                df["BBWidth"] = ((df["Upper Bollinger Band"] - df["Lower Bollinger Band"]) / df["SMA"] * 100)
+
+                # Bollinger Bands Plot
+                fig = go.Figure()
+                fig.add_trace(go.Scatter(x=df.index, y=df["Adj Close"], mode='lines', name='Adj Close'))
+                fig.add_trace(go.Scatter(x=df.index, y=df["Upper Bollinger Band"], mode='lines', name='Upper Bollinger Band', line=dict(color='red')))
+                fig.add_trace(go.Scatter(x=df.index, y=df["Lower Bollinger Band"], mode='lines', name='Lower Bollinger Band', line=dict(color='blue')))
+                fig.add_trace(go.Scatter(x=df.index, y=df["Adj Close"].rolling(20).mean(), mode='lines', name='Mean Average', line=dict(color='orange', dash='dash')))
+                fig.update_layout(title=f"{symbol} Bollinger Bands",
+                                xaxis_title="Date",
+                                yaxis_title="Price")
+                st.plotly_chart(fig)
+
+                # BB Width Plot
+                fig = go.Figure()
+                fig.add_trace(go.Scatter(x=df.index, y=df["BBWidth"], mode='lines', name='BB Width', line=dict(color='black')))
+                fig.add_trace(go.Scatter(x=df.index, y=df["BBWidth"].rolling(20).mean(), mode='lines', name='200 Moving Average', line=dict(color='darkblue')))
+                fig.update_layout(title="Bollinger Bands Width",
+                                xaxis_title="Date",
+                                yaxis_title="BB Width")
+                st.plotly_chart(fig)
+
+
+
         if pred_option_Technical_Indicators == "Breadth Indicator":
             pass
         if pred_option_Technical_Indicators == "Candle Absolute Returns":
