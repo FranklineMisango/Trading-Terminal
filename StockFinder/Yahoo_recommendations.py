4e9a6cc79b05c095b3e6aa3a6e98135a474c94d2
misango <franklinemisango@gmail.com>
2024-09-08 11:49:23 +0800

    recode the finder --> yahoo

--- StockFinder/Yahoo_recommendations.py
+++ StockFinder/Yahoo_recommendations.py
@@ -2,4 +2,5 @@ from langchain_core.tools import tool
 
 @tool
 def tool_yahoo_recommendation_pipeline():
+    '''This tool allows you to screen for stocks based on Yahoo recommendations'''
     pass
