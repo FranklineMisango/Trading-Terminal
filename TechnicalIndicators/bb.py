c88d5fb23474f10479b449de9bbad17d33684e71
Etsehiwot Ayalew Bezabih <etseab@Etsehiwots-MacBook-Air.local>
2024-10-03 22:02:59 +0800

    modified bb's name from bba to bb

---
+++ TechnicalIndicators/bb.py
@@ -0,0 +1,105 @@
+import datetime as dt
+import yfinance as yf
+import streamlit as st
+import plotly.graph_objects as go
+from langchain_core.tools import tool
+
+
+@tool
+def tool_bb(ticker:str, start_date: dt.time, end_date: dt.time):
+    ''' This tool plots The Bollinger Bands (BB) of a stock along with the stock's closing price.'''
+    symbol = ticker
+    start = start_date
+    end = end_date
+
+    # Read data
+    df = yf.download(symbol, start, end)
+    df["VolumePositive"] = df["Open"] < df["Adj Close"]
+
+    n = 20
+    MA = pd.Series(df["Adj Close"].rolling(n).mean())
+    STD = pd.Series(df["Adj Close"].rolling(n).std())
+    bb1 = MA + 2 * STD
+    df["Upper Bollinger Band"] = pd.Series(bb1)
+    bb2 = MA - 2 * STD
+    df["Lower Bollinger Band"] = pd.Series(bb2)
+
+    # Bollinger Bands Plot
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=df.index, y=df["Adj Close"], mode='lines', name='Adj Close'))
+    fig.add_trace(go.Scatter(x=df.index, y=df["Upper Bollinger Band"], mode='lines', name='Upper Bollinger Band', line=dict(color='red')))
+    fig.add_trace(go.Scatter(x=df.index, y=df["Lower Bollinger Band"], mode='lines', name='Lower Bollinger Band', line=dict(color='blue')))
+    fig.update_layout(title=f"{symbol} Bollinger Bands",
+                    xaxis_title="Date",
+                    yaxis_title="Price")
+    st.plotly_chart(fig)
+
+    # Candlestick Chart with Bollinger Bands
+    fig = go.Figure(data=[go.Candlestick(x=df.index,
+                    open=df['Open'],
+                    high=df['High'],
+                    low=df['Low'],
+                    close=df['Close'], name='Candlestick')])
+    fig.add_trace(go.Scatter(x=df.index, y=df["Upper Bollinger Band"], mode='lines', name='Upper Bollinger Band', line=dict(color='red')))
+    fig.add_trace(go.Scatter(x=df.index, y=df["Lower Bollinger Band"], mode='lines', name='Lower Bollinger Band', line=dict(color='blue')))
+    fig.update_layout(title="Candlestick Chart with Bollinger Bands",
+                    xaxis_title="Date",
+                    yaxis_title="Price")
+    st.plotly_chart(fig)
+
+    # Volume Chart
+    fig = go.Figure()
+    fig.add_trace(go.Bar(x=df.index, y=df["Volume"], name='Volume', marker_color=df.VolumePositive.map({True: "green", False: "red"})))
+    fig.update_layout(title="Volume",
+                    xaxis_title="Date",
+                    yaxis_title="Volume")
+    st.plotly_chart(fig) 
+
+
+    def norm_bb(ticker, start_date, end_date):
+    symbol = ticker
+    start = start_date
+    end = end_date
+
+    # Read data
+    df = yf.download(symbol, start, end)
+    df["VolumePositive"] = df["Open"] < df["Adj Close"]
+
+    n = 20
+    MA = pd.Series(df["Adj Close"].rolling(n).mean())
+    STD = pd.Series(df["Adj Close"].rolling(n).std())
+    bb1 = MA + 2 * STD
+    df["Upper Bollinger Band"] = pd.Series(bb1)
+    bb2 = MA - 2 * STD
+    df["Lower Bollinger Band"] = pd.Series(bb2)
+
+    # Bollinger Bands Plot
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=df.index, y=df["Adj Close"], mode='lines', name='Adj Close'))
+    fig.add_trace(go.Scatter(x=df.index, y=df["Upper Bollinger Band"], mode='lines', name='Upper Bollinger Band', line=dict(color='red')))
+    fig.add_trace(go.Scatter(x=df.index, y=df["Lower Bollinger Band"], mode='lines', name='Lower Bollinger Band', line=dict(color='blue')))
+    fig.update_layout(title=f"{symbol} Bollinger Bands",
+                    xaxis_title="Date",
+                    yaxis_title="Price")
+    st.plotly_chart(fig)
+
+    # Candlestick Chart with Bollinger Bands
+    fig = go.Figure(data=[go.Candlestick(x=df.index,
+                    open=df['Open'],
+                    high=df['High'],
+                    low=df['Low'],
+                    close=df['Close'], name='Candlestick')])
+    fig.add_trace(go.Scatter(x=df.index, y=df["Upper Bollinger Band"], mode='lines', name='Upper Bollinger Band', line=dict(color='red')))
+    fig.add_trace(go.Scatter(x=df.index, y=df["Lower Bollinger Band"], mode='lines', name='Lower Bollinger Band', line=dict(color='blue')))
+    fig.update_layout(title="Candlestick Chart with Bollinger Bands",
+                    xaxis_title="Date",
+                    yaxis_title="Price")
+    st.plotly_chart(fig)
+
+    # Volume Chart
+    fig = go.Figure()
+    fig.add_trace(go.Bar(x=df.index, y=df["Volume"], name='Volume', marker_color=df.VolumePositive.map({True: "green", False: "red"})))
+    fig.update_layout(title="Volume",
+                    xaxis_title="Date",
+                    yaxis_title="Volume")
+    st.plotly_chart(fig)  
--- app.py
+++ app.py
@@ -245,6 +245,7 @@ from TechnicalIndicators.atr import tool_atr, norm_atr
 from TechnicalIndicators.bp import tool_bp, norm_bp
 from TechnicalIndicators.bi import tool_bi, norm_bi
 from TechnicalIndicators.bb import tool_bb, norm_bb
+from TechnicalIndicators.bbw import tool_bbw, norm_bbw
 
 
 
@@ -273,7 +274,7 @@ tools = [tool_analyze_idb_rs_rating,tool_correlated_stocks, tool_growth_screener
          tool_ema,tool_ema_volume, tool_gann_lines_angles, tool_gmma,
          tool_macd,tool_mfi,tool_ma_high_low,tool_pvi, tool_pvt, tool_roc,tool_roi, tool_rsi,
          tool_rsi_bollinger_bands,tool_vwap,tool_wma,tool_wsma,tool_z_score, tool_accleration_bands,
-         tool_adl, tool_aroon, tool_adx, tool_atr, tool_bp, tool_bi, tool_bb
+         tool_adl, tool_aroon, tool_adx, tool_atr, tool_bp, tool_bi, tool_bb, tool_bbw
          ]
 
 
@@ -1676,45 +1677,7 @@ def main():
                 with col2:
                     end_date = st.date_input("End Date:")
                 if st.button("Check"):    
-                    symbol = ticker
-                    start = start_date
-                    end = end_date
-
-                    # Read data
-                    df = yf.download(symbol, start, end)
-
-                    n = 20
-                    MA = pd.Series(df["Adj Close"].rolling(n).mean())
-                    STD = pd.Series(df["Adj Close"].rolling(n).std())
-                    bb1 = MA + 2 * STD
-                    df["Upper Bollinger Band"] = pd.Series(bb1)
-                    bb2 = MA - 2 * STD
-                    df["Lower Bollinger Band"] = pd.Series(bb2)
-                    df["SMA"] = df["Adj Close"].rolling(n).mean()
-
-                    df["BBWidth"] = ((df["Upper Bollinger Band"] - df["Lower Bollinger Band"]) / df["SMA"] * 100)
-
-                    # Bollinger Bands Plot
-                    fig = go.Figure()
-                    fig.add_trace(go.Scatter(x=df.index, y=df["Adj Close"], mode='lines', name='Adj Close'))
-                    fig.add_trace(go.Scatter(x=df.index, y=df["Upper Bollinger Band"], mode='lines', name='Upper Bollinger Band', line=dict(color='red')))
-                    fig.add_trace(go.Scatter(x=df.index, y=df["Lower Bollinger Band"], mode='lines', name='Lower Bollinger Band', line=dict(color='blue')))
-                    fig.add_trace(go.Scatter(x=df.index, y=df["Adj Close"].rolling(20).mean(), mode='lines', name='Mean Average', line=dict(color='orange', dash='dash')))
-                    fig.update_layout(title=f"{symbol} Bollinger Bands",
-                                    xaxis_title="Date",
-                                    yaxis_title="Price")
-                    st.plotly_chart(fig)
-
-                    # BB Width Plot
-                    fig = go.Figure()
-                    fig.add_trace(go.Scatter(x=df.index, y=df["BBWidth"], mode='lines', name='BB Width', line=dict(color='black')))
-                    fig.add_trace(go.Scatter(x=df.index, y=df["BBWidth"].rolling(20).mean(), mode='lines', name='200 Moving Average', line=dict(color='darkblue')))
-                    fig.update_layout(title="Bollinger Bands Width",
-                                    xaxis_title="Date",
-                                    yaxis_title="BB Width")
-                    st.plotly_chart(fig)
-
-
+                    norm_bbw(ticker, start_date, end_date)
 
             if pred_option_Technical_Indicators == "Breadth Indicator":
                 st.success("This program allows you to view the Breadth Indicator of a ticker over time")
